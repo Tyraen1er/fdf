3da6e56076b3de6423992a0e4ad85e1450a86229
Emmanuel FERRANDINI <emmanuel.ferrandini@yahoo.fr>
2017-03-18 11:19:51 +0100

    bonjour Ca marche pour les 2 3 mais pas les 3 3

--- src/sup.c
+++ src/sup.c
@@ -24,7 +24,6 @@ int		*matrice_multi(int *fst, int *scd)
 	int	a;
 
 	y = -1;
-	a = 0;
 	if (fst[0] != scd[1])
 		return (NULL);
 	ret = (int*)malloc(sizeof(int) * (fst[1] * scd[0] + 2));
@@ -36,16 +35,18 @@ int		*matrice_multi(int *fst, int *scd)
 	{
 		while (++x < scd[0] && (a = -1))
 		{
-			printf("debut case\n");
-			while (++a < scd[0])
+//			printf("debut case\n");
+			while (++a < fst[0])
 			{
-				printf("\nx = %d\ny = %d\na = %d\n", x, y, a);
-				printf("%d * %d\n" ,fst[a + y + 2] , scd[a * scd[0] + x + 2]);
-				printf("case 1 = %d\ncase 2 = %d\n", a + y * fst[0] + 2, a * scd[0] + x + 2);
-				printf("%d, ", fst[a + y * fst[0]] * scd[a * scd[0] + x]);
-				ret[2 + x + y * scd[0]] = fst[a + y * fst[0] + 2] * scd[2 + a * scd[0] + x];
+//				printf("\nx = %d\ny = %d\na = %d\n", x, y, a);
+//				printf("%d * %d\n" ,fst[a + y * fst[0] + 2] , scd[a * scd[0] + x + 2]);
+//				printf("position case 1 = %d\nposition case 2 = %d\n", a + y * fst[0] + 2, a * scd[0] + x + 2);
+//				printf("case qu on remplie : %d\n", 2 + x + y * scd[0]);
+//				printf("%d, ", fst[a + y * fst[0]] * scd[2 + a * scd[0] + x]);
+				ret[2 + x + y * scd[0]] += fst[a + y * fst[0] + 2] * scd[a * scd[0] + x + 2];
+//				printf("ret = %d\n", ret[2 + x + y * scd[0]]);
 			}
-			printf("\nune case finie\n\n");
+//			printf("\nune case finie\n\n");
 		}
 	}
 	printf("fin multiplication\n\n");
@@ -59,19 +60,22 @@ int main()
 	int	scd[8];
 	int	*ret;
 
-	fst[0] = 2;
+	fst[0] = 3;
 	fst[1] = 3;
 	
 	fst[2] = 3;
 	fst[3] = 4;
-	fst[4] = 2;
-	fst[5] = 7;
-	fst[6] = 1;
+	fst[4] = 5;
+	fst[5] = 2;
+	fst[6] = 7;
+	fst[7] = 2;
+	fst[7] = 1;
 	fst[7] = 9;
+	fst[7] = 6;
 
 
 	scd[0] = 3;
-	scd[1] = 2;
+	scd[1] = 3;
 	
 	scd[2] = 2;
 	scd[3] = 7;
@@ -79,6 +83,9 @@ int main()
 	scd[5] = 5;
 	scd[6] = -3;
 	scd[7] = 8;
+	scd[7] = 18;
+	scd[7] = 1;
+	scd[7] = -4;
 
 	ret = matrice_multi(fst, scd);
 	a = 0;
